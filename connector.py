2c9e8f33d3831eb96e46c901d80286a081478582
Boris.Dergachov <admin@froexilize.com>
2019-05-22 13:07:08 +0300

    api get_jsons

--- connector.py
+++ connector.py
@@ -16,9 +16,11 @@ app.config['REDIS_HOST'] = 'localhost'
 app.config['REDIS_PORT'] = 6379
 app.config['REDIS_DB'] = 0
 
+
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/api/', methods=['GET'])
 def api_call():
@@ -29,14 +31,18 @@ def api_call():
 <h1>API call</h1>
 '''
 
+
 @app.route('/redis_test/', methods=['GET'])
 def index():
-    redis_store.set('potato', 'Don\'t fuck me')
     response = redis_store.get('potato')
-    if response == None:
+    if response is None:
         return 'Fuck!'
     return response
 
+@app.route('/api/get_jsons', methods=['GET'])
+def api_get_jsons():
+    response = redis_store.hscan('uid_to_json', cursor=0)
+    return json.dumps(response[1], sort_keys=False, indent=4)
 
 def imageToJson(path: str):
     import base64
@@ -44,10 +50,8 @@ def imageToJson(path: str):
     with(open(path, 'rb')) as file:
         buffer = base64.b64encode(file.read())
         file.close()
-                    
     b64buffer = {'b64': binary.decode('ascii')}
     return b64buffer
-
 
 
 @app.route('/api/get_counters', methods=['GET'])
@@ -65,6 +69,7 @@ def api_get_counters():
             user_data=c.binary
         )
 
+
 @app.route('/api/get_penalties', methods=['GET'])
 def api_test():
     if request.method == 'GET':
@@ -78,6 +83,7 @@ def api_test():
                 jsons.append(json.dumps(data, sort_keys=False, indent=4))
     return json.dumps(jsons, sort_keys=False, indent=4)
 
+
 @app.route('/api/get_penalty', methods=['GET'])
 def api_get_penalty():
     if request.method == 'GET':
@@ -85,23 +91,21 @@ def api_get_penalty():
         root_dir = './tars'
         tars_path = []
         counter = int(0)
-        for r,d,f in os.walk(root_dir):
+        for r, d, f in os.walk(root_dir):
             for folder in d:
                 if counter == id:
                     json_file = os.path.join(r, folder, "prizma.json")
                     image_file = os.path.join(r, folder, "m.jpg")
-                    
-                    #encoded base64 str
+                    # encoded base64 str
                     b64str = imageToJson(path=image_file)
-              
                     with open(json_file, encoding='utf-8') as f:
                         json_data = json.load(f)
                     json_data['picture'] = b64str
                     return json.dumps(json_data, sort_keys=False, indent=4)
                 counter = counter + 1
     return jsonify(
-        error_code = -1,
-        error_msg = "Not found"
+        error_code=-1,
+        error_msg="Not found"
     )
 
 if __name__ == '__main__':
